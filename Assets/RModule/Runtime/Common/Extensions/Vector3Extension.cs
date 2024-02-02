c6da8b35ad8ad12ee27475ca928b530d9cd3a151
Aleksandr <alexandr_isaew@mail.ru>
2024-02-02 13:23:35 +0300

    Add new extension

---
+++ Assets/RModule/Runtime/Common/Extensions/Vector3Extension.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RModule.Runtime.Extensions {
+	public static class Vector3Extension {
+
+		/// <summary>Calculates angle between 2 vectors. Angle increases clockwise.</summary>
+		/// <param name="from"> Starting Vector with 0°, like Vector3.up</param>
+		/// <param name="clockwise">Clockwise.</param>
+		/// <param name="o">Starting position of vectors.</param>
+		/// <returns>Angle between 0° and 360° from.</returns>
+		public static float CalculateAngle1(Vector3 to, Vector3 from, bool clockwise = true) {
+			return Quaternion.FromToRotation(clockwise ? to - from : from - to, from).eulerAngles.z;
+		}
+	}
+}
