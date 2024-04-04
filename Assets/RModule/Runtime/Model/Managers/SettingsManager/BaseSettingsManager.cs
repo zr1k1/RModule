6830a199417aaf387992c80afb98dccb106d149e
Aleksandr <alexandr_isaew@mail.ru>
2024-04-04 17:31:22 +0300

    Add get debug config values from settings manager

--- Assets/RModule/Runtime/Model/Managers/SettingsManager/BaseSettingsManager.cs
+++ Assets/RModule/Runtime/Model/Managers/SettingsManager/BaseSettingsManager.cs
@@ -111,4 +111,8 @@ public abstract class BaseSettingsManager<PurchasableGameItem, Placement, Option
 	public virtual T1 GetSetting<T1>(OptionalCrossPlatformAppConfigSetting enumType) {
 		return ((IValueGetter<OptionalCrossPlatformAppConfigSetting>)_appConfigData).GetValue<T1>(enumType);
 	}
+
+	public virtual T1 GetSetting<T1>(OptionaDebugSetting enumType) {
+		return ((IValueGetter<OptionaDebugSetting>)_debugConfig).GetValue<T1>(enumType);
+	}
 }
