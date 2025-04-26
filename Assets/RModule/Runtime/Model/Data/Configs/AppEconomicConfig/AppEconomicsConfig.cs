f29a444c59cdf6bc5b90cc6fb2cd913a663a27be
Aleksandr <alexandr_isaew@mail.ru>
2025-04-26 19:32:49 +0300

    Refactor

--- Assets/RModule/Runtime/Model/Data/Configs/AppEconomicConfig/AppEconomicsConfig.cs
+++ Assets/RModule/Runtime/Model/Data/Configs/AppEconomicConfig/AppEconomicsConfig.cs
@@ -5,5 +5,5 @@ using RModule.Runtime.Data.Configs;
 // Create your "CustomGameItem" enum with placements for app
 // Create a class and inherit from AppEconomics<CustomGameItem>
 // For additional conditions to get price value create SomeHardPriceConfig and inherit PriceConfig. drug in inspector
-public class AppEconomicsConfig<PurchasableGameItem> : BaseConfig<PurchasableGameItem> where PurchasableGameItem : Enum {
+public class AppEconomicsConfig<TReceivedGameItem> : BaseConfig<TReceivedGameItem> where TReceivedGameItem : Enum {
 }
