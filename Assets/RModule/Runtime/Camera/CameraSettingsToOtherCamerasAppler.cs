56c975b7e61033a3e5a7ad2713c31e27397c9351
Aleksandr <alexandr_isaew@mail.ru>
2025-05-14 17:36:58 +0300

    Improve CameraSettingsToOtherCamerasAppler

--- Assets/RModule/Runtime/Camera/CameraSettingsToOtherCamerasAppler.cs
+++ Assets/RModule/Runtime/Camera/CameraSettingsToOtherCamerasAppler.cs
@@ -7,11 +7,25 @@ public class CameraSettingsToOtherCamerasAppler : MonoBehaviour {
     [SerializeField] Camera _main = default;
     [SerializeField] List<Camera> _camerasToApply = default;
 
-    public void UpdateSettingsFromMainCamera() {
+	bool _updateCameraView;
 
+	public void UpdateSettingsFromMainCamera() {
+		_updateCameraView = true;
+	}
+
+	private void LateUpdate() {
+		if (_updateCameraView) {
+			_updateCameraView = false;
+			UpdateCameraView();
+		}
+	}
+
+	void UpdateCameraView() {
 		foreach (var camera in _camerasToApply) {
 			camera.orthographicSize = _main.orthographicSize;
 			camera.transform.position = _main.transform.position;
+			camera.gameObject.SetActive(false);
+			camera.gameObject.SetActive(true);
 		}
 	}
 }
