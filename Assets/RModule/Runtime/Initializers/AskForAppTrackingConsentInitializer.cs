19e315a1186dfb89c4447ce56d248787597fa586
Aleksandr <alexandr_isaew@mail.ru>
2026-08-20 18:54:19 +0300

    Use only native AskForAppTrackingConsent

--- Assets/RModule/Runtime/Initializers/AskForAppTrackingConsentInitializer.cs
+++ Assets/RModule/Runtime/Initializers/AskForAppTrackingConsentInitializer.cs
@@ -4,20 +4,14 @@ using UnityEngine;
 using MTUnityCore.Runtime.Plugins;
 #endif
 
-public class AskForAppTrackingConsentInitializer : Initializer
-{
+public class AskForAppTrackingConsentInitializer : Initializer {
 
-	public override IEnumerator Initialize()
-	{
+	public override IEnumerator Initialize() {
 		Debug.Log("AskForAppTrackingConsentInitializer : TryAskForAppTrackingConsent");
 
 #if USE_MTUNITYCORE
-#if USE_APPODEAL
-		Debug.Log("AskForAppTrackingConsentInitializer : Use Appodeal AskForAppTrackingConsent logic");
-#else
 		Debug.Log("AskForAppTrackingConsentInitializer : Use Native AskForAppTrackingConsent");
 		yield return AppTrackingConsentManager.AskForAppTrackingConsent();
-#endif
 #endif
 		yield return null;
 	}
