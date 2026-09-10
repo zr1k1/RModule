de8231d0eba9baf7743d6abe9e91956841f80db0
Aleksandr <alexandr_isaew@mail.ru>
2026-09-10 18:02:48 +0300

    HardwareBtnsEventsManager for new unity input system

---
+++ Assets/RModule/Runtime/Input/HardwareBtnsEventsManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HardwareBtnsEventsManager : SingletonMonoBehaviour<HardwareBtnsEventsManager> {
+
+	public delegate void HardwareBackBtnTappedDelegate();
+	public static event HardwareBackBtnTappedDelegate HardwareBackBtnTappedEvent;
+
+	// ---------------------------------------------------------------
+	// GameObject lifecycle
+
+	void Update() {
+		if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame) {
+			HardwareBackBtnTappedEvent?.Invoke();
+		}
+	}
+
+	public override bool IsInitialized() {
+		return Instance != null;
+	}
+}
