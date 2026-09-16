0de8f93404e5fd61e6a478d1631dd8069ccb4071
Aleksandr <alexandr_isaew@mail.ru>
2026-09-16 16:07:49 +0300

    Improved LocalzedImage

--- Assets/RModule/Runtime/Model/Localization/Scripts/LocalizedImage.cs
+++ Assets/RModule/Runtime/Model/Localization/Scripts/LocalizedImage.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -13,21 +13,25 @@ public class LocalizedImage : MonoBehaviour {
 	[SerializeField] bool _useAddressables;
 	[SerializeField] bool _useResources = default;
 	[SerializeField] string _imageBaseName = default;
+	[SerializeField] List<Image> _alsoApplyImages = default;
 
 	// Private vars
+	Image _image;
 	Sprite _localizedSprite;
 
 	IEnumerator Start() {
 		yield return LocalizationManager.WaitForInstanceCreatedAndInitialized();
 
-		var image = GetComponent<Image>();
-		if (image == null)
+		_image = GetComponent<Image>();
+		if (_image == null) {
+			Debug.LogError($"GameObject is not have Image component!");
 			yield break;
+		}
 
 		if (_localizedImages != null && _localizedImages.Count > 0) {
 			var currentLanguage = LocalizationManager.Instance.CurrentLanguage;
 			_localizedImages.TryGetValue(currentLanguage, out var localizedSprite);
-			image.sprite = localizedSprite;
+			SetSprite(localizedSprite);
 		}
 
 		if (_useAddressables) {
@@ -35,17 +39,36 @@ public class LocalizedImage : MonoBehaviour {
 			string fullAssetName = $"{_imageBaseName}{LocalizationManager.LanguageIdStringForType(currentLanguage)}";
 			Addressables.LoadAssetAsync<Sprite>(fullAssetName).Completed += handle => {
 				if (handle.Status == AsyncOperationStatus.Succeeded) {
-					image.enabled = true;
-					_localizedSprite = handle.Result;
-					image.sprite = _localizedSprite;
+					SetSprite(handle.Result);
 				}
 			};
 		} else if (_useResources) {
 			var currentLanguage = LocalizationManager.Instance.CurrentLanguage;
 			string fullAssetName = $"{_imageBaseName}{LocalizationManager.LanguageIdStringForType(currentLanguage)}";
 			var sprite = Resources.Load<Sprite>(fullAssetName);
-			image.enabled = true;
-			image.sprite = sprite;
+			SetSprite(sprite);
+		}
+	}
+
+	void SetSprite(Sprite sprite) {
+		if (_image == null) {
+			Debug.LogError($"GameObject is not have Image component!");
+			return;
+		}
+		_image.enabled = true;
+		_localizedSprite = sprite;
+		_image.sprite = _localizedSprite;
+		AlsoApplyToImages(sprite);
+	}
+
+	void AlsoApplyToImages(Sprite sprite) {
+		if (_localizedSprite != null)
+			foreach (var img in _alsoApplyImages) {
+				img.enabled = true;
+				img.sprite = sprite;
+			}
+		else {
+			Debug.LogError($"LocalizedImage : _localizedSprite is null!");
 		}
 	}
 
